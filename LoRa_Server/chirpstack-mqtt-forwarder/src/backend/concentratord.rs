3f38d05d4a8cec440953fc358fb20030e8e45188
Okacha Amraouy <okacha.amraouy@usmba.ac.ma>
2024-06-11 13:23:44 +0100

    Add files via upload

---
+++ LoRa_Server/chirpstack-mqtt-forwarder/src/backend/concentratord.rs
@@ -0,0 +1,300 @@
+use std::io::Cursor;
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::Duration;
+
+use anyhow::Result;
+use async_trait::async_trait;
+use chirpstack_api::gw;
+use log::{debug, error, info, trace, warn};
+use prost::Message;
+use tokio::task;
+
+use super::Backend as BackendTrait;
+use crate::config::Configuration;
+use crate::metadata;
+use crate::mqtt::{send_gateway_stats, send_tx_ack, send_uplink_frame};
+
+pub struct Backend {
+    gateway_id: String,
+    ctx: zmq::Context,
+    cmd_url: String,
+    cmd_sock: Mutex<zmq::Socket>,
+}
+
+impl Backend {
+    pub async fn setup(conf: &Configuration) -> Result<Self> {
+        info!("Setting up ChirpStack Concentratord backend");
+
+        let zmq_ctx = zmq::Context::new();
+
+        info!(
+            "Connecting to Concentratord event API, event_url: {}",
+            conf.backend.concentratord.event_url
+        );
+        let event_sock = zmq_ctx.socket(zmq::SUB)?;
+        event_sock.connect(&conf.backend.concentratord.event_url)?;
+        event_sock.set_subscribe("".as_bytes())?;
+
+        info!(
+            "Connecting to Concentratord command API, command_url: {}",
+            conf.backend.concentratord.command_url
+        );
+        let cmd_sock = zmq_ctx.socket(zmq::REQ)?;
+        cmd_sock.connect(&conf.backend.concentratord.command_url)?;
+
+        info!("Reading gateway id");
+
+        // send 'gateway_id' command with empty payload.
+        cmd_sock.send("gateway_id", zmq::SNDMORE)?;
+        cmd_sock.send("", 0)?;
+
+        // set poller so that we can timeout after 100ms
+        let mut items = [cmd_sock.as_poll_item(zmq::POLLIN)];
+        zmq::poll(&mut items, 100)?;
+        if !items[0].is_readable() {
+            return Err(anyhow!("Could not read gateway id"));
+        }
+        let gateway_id = cmd_sock.recv_bytes(0)?;
+        let gateway_id = hex::encode(gateway_id);
+
+        info!("Received gateway id, gateway_id: {}", gateway_id);
+
+        tokio::spawn({
+            let forward_crc_ok = conf.backend.filters.forward_crc_ok;
+            let forward_crc_invalid = conf.backend.filters.forward_crc_invalid;
+            let forward_crc_missing = conf.backend.filters.forward_crc_missing;
+            let filters = lrwn_filters::Filters {
+                dev_addr_prefixes: conf.backend.filters.dev_addr_prefixes.clone(),
+                join_eui_prefixes: conf.backend.filters.join_eui_prefixes.clone(),
+            };
+
+            async move {
+                event_loop(
+                    event_sock,
+                    filters,
+                    forward_crc_ok,
+                    forward_crc_invalid,
+                    forward_crc_missing,
+                )
+                .await;
+            }
+        });
+
+        Ok(Backend {
+            gateway_id,
+            ctx: zmq_ctx,
+            cmd_url: conf.backend.concentratord.command_url.clone(),
+            cmd_sock: Mutex::new(cmd_sock),
+        })
+    }
+
+    fn send_command(&self, cmd: &str, b: &[u8]) -> Result<Vec<u8>> {
+        let res = || -> Result<Vec<u8>> {
+            let cmd_sock = self.cmd_sock.lock().unwrap();
+            cmd_sock.send(cmd, zmq::SNDMORE)?;
+            cmd_sock.send(b, 0)?;
+
+            // set poller so that we can timeout after 100ms
+            let mut items = [cmd_sock.as_poll_item(zmq::POLLIN)];
+            zmq::poll(&mut items, 100)?;
+            if !items[0].is_readable() {
+                return Err(anyhow!("Could not read down response"));
+            }
+
+            // red tx ack response
+            let resp_b: &[u8] = &cmd_sock.recv_bytes(0)?;
+            Ok(resp_b.to_vec())
+        }();
+
+        if res.is_err() {
+            loop {
+                // Reconnect the CMD socket in case we received an error.
+                // In case there was an issue with receiving data from the socket, it could mean
+                // it is in a 'dirty' state. E.g. due to the error we did not read the full
+                // response.
+                if let Err(e) = self.reconnect_cmd_sock() {
+                    error!(
+                        "Re-connecting to Concentratord command API error, error: {}",
+                        e
+                    );
+                    sleep(Duration::from_secs(1));
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        res
+    }
+
+    fn reconnect_cmd_sock(&self) -> Result<()> {
+        warn!(
+            "Re-connecting to Concentratord command API, command_url: {}",
+            self.cmd_url
+        );
+        let mut cmd_sock = self.cmd_sock.lock().unwrap();
+        *cmd_sock = self.ctx.socket(zmq::REQ)?;
+        cmd_sock.connect(&self.cmd_url)?;
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl BackendTrait for Backend {
+    async fn get_gateway_id(&self) -> Result<String> {
+        Ok(self.gateway_id.clone())
+    }
+
+    async fn send_downlink_frame(&self, pl: &gw::DownlinkFrame) -> Result<()> {
+        info!("Sending downlink frame, downlink_id: {}", pl.downlink_id);
+
+        let tx_ack = {
+            let b = pl.encode_to_vec();
+            let resp_b = self.send_command("down", &b)?;
+            gw::DownlinkTxAck::decode(&mut Cursor::new(resp_b))?
+        };
+
+        let ack_items: Vec<String> = tx_ack
+            .items
+            .iter()
+            .map(|i| i.status().as_str_name().to_string())
+            .collect();
+
+        info!(
+            "Received ack, items: {:?}, downlink_id: {}",
+            ack_items, pl.downlink_id
+        );
+
+        send_tx_ack(&tx_ack).await
+    }
+
+    async fn send_configuration_command(&self, pl: &gw::GatewayConfiguration) -> Result<()> {
+        info!("Sending configuration command, version: {}", pl.version);
+
+        let b = pl.encode_to_vec();
+        let _ = self.send_command("config", &b)?;
+
+        Ok(())
+    }
+}
+
+async fn event_loop(
+    event_sock: zmq::Socket,
+    filters: lrwn_filters::Filters,
+    forward_crc_ok: bool,
+    forward_crc_invalid: bool,
+    forward_crc_missing: bool,
+) {
+    trace!("Starting event loop");
+    let event_sock = Arc::new(Mutex::new(event_sock));
+
+    loop {
+        let res = task::spawn_blocking({
+            let event_sock = event_sock.clone();
+
+            move || -> Result<Vec<Vec<u8>>> {
+                let event_sock = event_sock.lock().unwrap();
+
+                // set poller so that we can timeout after 100ms
+                let mut items = [event_sock.as_poll_item(zmq::POLLIN)];
+                zmq::poll(&mut items, 100)?;
+                if !items[0].is_readable() {
+                    return Ok(vec![]);
+                }
+
+                let msg = event_sock.recv_multipart(0)?;
+                if msg.len() != 2 {
+                    return Err(anyhow!("Event must have two frames"));
+                }
+                Ok(msg)
+            }
+        })
+        .await;
+
+        match res {
+            Ok(Ok(msg)) => {
+                if msg.len() != 2 {
+                    continue;
+                }
+
+                if let Err(err) = handle_event_msg(
+                    &msg[0],
+                    &msg[1],
+                    &filters,
+                    forward_crc_ok,
+                    forward_crc_invalid,
+                    forward_crc_missing,
+                )
+                .await
+                {
+                    error!("Handle event error: {}", err);
+                    continue;
+                }
+            }
+            Ok(Err(err)) => {
+                error!("Receive event error, error: {}", err);
+                continue;
+            }
+            Err(err) => {
+                error!("{}", err);
+                continue;
+            }
+        }
+    }
+}
+
+async fn handle_event_msg(
+    event: &[u8],
+    pl: &[u8],
+    filters: &lrwn_filters::Filters,
+    forward_crc_ok: bool,
+    forward_crc_invalid: bool,
+    forward_crc_missing: bool,
+) -> Result<()> {
+    let event = String::from_utf8(event.to_vec())?;
+    let pl = Cursor::new(pl.to_vec());
+
+    match event.as_str() {
+        "up" => {
+            let pl = gw::UplinkFrame::decode(pl)?;
+            if let Some(rx_info) = &pl.rx_info {
+                if !((rx_info.crc_status() == gw::CrcStatus::CrcOk && forward_crc_ok)
+                    || (rx_info.crc_status() == gw::CrcStatus::BadCrc && forward_crc_invalid)
+                    || (rx_info.crc_status() == gw::CrcStatus::NoCrc && forward_crc_missing))
+                {
+                    debug!(
+                        "Ignoring uplink frame because of forward_crc_ flags, uplink_id: {}",
+                        pl.rx_info.as_ref().map(|v| v.uplink_id).unwrap_or_default(),
+                    );
+                    return Ok(());
+                }
+            }
+
+            if lrwn_filters::matches(&pl.phy_payload, filters) {
+                info!(
+                    "Received uplink frame, uplink_id: {}",
+                    pl.rx_info.as_ref().map(|v| v.uplink_id).unwrap_or_default(),
+                );
+                send_uplink_frame(&pl).await?;
+            } else {
+                debug!(
+                    "Ignoring uplink frame because of dev_addr and join_eui filters, uplink_id: {}",
+                    pl.rx_info.as_ref().map(|v| v.uplink_id).unwrap_or_default()
+                );
+            }
+        }
+        "stats" => {
+            let mut pl = gw::GatewayStats::decode(pl)?;
+            info!("Received gateway stats");
+            pl.metadata.extend(metadata::get().await?);
+            send_gateway_stats(&pl).await?;
+        }
+        _ => {
+            return Err(anyhow!("Unexpected event: {}", event));
+        }
+    }
+
+    Ok(())
+}
---
+++ LoRa_Server/chirpstack-mqtt-forwarder/src/backend/semtech_udp/mod.rs
@@ -0,0 +1,508 @@
+use std::collections::HashMap;
+use std::io::Cursor;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::{Duration, SystemTime};
+
+use anyhow::Result;
+use async_trait::async_trait;
+use chirpstack_api::gw;
+use log::{debug, info, trace, warn};
+use prost::Message;
+use tokio::net::UdpSocket;
+use tokio::sync::{Mutex, RwLock};
+
+use super::Backend as BackendTrait;
+use crate::config::Configuration;
+use crate::metadata;
+use crate::mqtt::{send_gateway_stats, send_tx_ack, send_uplink_frame};
+
+mod structs;
+
+struct State {
+    socket: UdpSocket,
+    gateway_id: Mutex<String>,
+    downlink_cache: RwLock<HashMap<u16, DownlinkCache>>,
+    pull_addr: RwLock<Option<SocketAddr>>,
+    stats: Mutex<Stats>,
+    time_fallback_enabled: bool,
+    forward_crc_ok: bool,
+    forward_crc_invalid: bool,
+    forward_crc_missing: bool,
+    filters: lrwn_filters::Filters,
+}
+
+#[derive(Clone)]
+struct DownlinkCache {
+    expire: SystemTime,
+    frame: gw::DownlinkFrame,
+    ack_items: Vec<gw::DownlinkTxAckItem>,
+    index: usize,
+}
+
+#[derive(Default)]
+struct Stats {
+    pub rx_count: u32,
+    pub tx_count: u32,
+    pub rx_per_freq_count: HashMap<u32, u32>,
+    pub tx_per_freq_count: HashMap<u32, u32>,
+    pub rx_per_modulation_count: HashMap<Vec<u8>, u32>,
+    pub tx_per_modulation_count: HashMap<Vec<u8>, u32>,
+    pub tx_status_count: HashMap<String, u32>,
+}
+
+impl Stats {
+    fn count_uplink(&mut self, pl: &gw::UplinkFrame) -> Result<()> {
+        let tx_info = pl
+            .tx_info
+            .as_ref()
+            .ok_or_else(|| anyhow!("tx_info is missing"))?;
+        let modulation = tx_info
+            .modulation
+            .as_ref()
+            .ok_or_else(|| anyhow!("modulation is missing"))?;
+
+        let b = modulation.encode_to_vec();
+        self.rx_count += 1;
+        self.rx_per_freq_count
+            .entry(tx_info.frequency)
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+        self.rx_per_modulation_count
+            .entry(b)
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+
+        Ok(())
+    }
+
+    fn count_downlink(&mut self, pl: &gw::DownlinkFrame, ack: &gw::DownlinkTxAck) -> Result<()> {
+        for (i, v) in ack.items.iter().enumerate() {
+            if v.status() == gw::TxAckStatus::Ignored {
+                continue;
+            }
+
+            let status = v.status().as_str_name().to_string();
+            self.tx_status_count
+                .entry(status)
+                .and_modify(|v| *v += 1)
+                .or_insert(1);
+
+            if v.status() == gw::TxAckStatus::Ok && i < pl.items.len() {
+                let item = pl
+                    .items
+                    .get(i)
+                    .ok_or_else(|| anyhow!("Invalid items index"))?;
+                let tx_info = item
+                    .tx_info
+                    .as_ref()
+                    .ok_or_else(|| anyhow!("tx_info is missing"))?;
+                let modulation = tx_info
+                    .modulation
+                    .as_ref()
+                    .ok_or_else(|| anyhow!("modulation is missing"))?;
+                let b = modulation.encode_to_vec();
+
+                self.tx_count += 1;
+                self.tx_per_freq_count
+                    .entry(tx_info.frequency)
+                    .and_modify(|v| *v += 1)
+                    .or_insert(1);
+                self.tx_per_modulation_count
+                    .entry(b)
+                    .and_modify(|v| *v += 1)
+                    .or_insert(1);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn export_stats(&mut self) -> Result<gw::GatewayStats> {
+        let mut stats = gw::GatewayStats {
+            rx_packets_received: self.rx_count,
+            rx_packets_received_ok: self.rx_count,
+            tx_packets_received: self.tx_count,
+            tx_packets_emitted: self.tx_count,
+            tx_packets_per_status: self.tx_status_count.clone(),
+            ..Default::default()
+        };
+
+        for (k, v) in self.rx_per_freq_count.iter() {
+            stats.rx_packets_per_frequency.insert(*k, *v);
+        }
+
+        for (k, v) in self.tx_per_freq_count.iter() {
+            stats.tx_packets_per_frequency.insert(*k, *v);
+        }
+
+        for (k, v) in self.rx_per_modulation_count.iter() {
+            let modulation = gw::Modulation::decode(&mut Cursor::new(k))?;
+            stats
+                .rx_packets_per_modulation
+                .push(gw::PerModulationCount {
+                    count: *v,
+                    modulation: Some(modulation),
+                });
+        }
+
+        for (k, v) in self.tx_per_modulation_count.iter() {
+            let modulation = gw::Modulation::decode(&mut Cursor::new(k))?;
+            stats
+                .tx_packets_per_modulation
+                .push(gw::PerModulationCount {
+                    count: *v,
+                    modulation: Some(modulation),
+                });
+        }
+
+        *self = Stats::default();
+
+        Ok(stats)
+    }
+}
+
+impl State {
+    async fn set_gateway_id(&self, gateway_id: &[u8]) {
+        let mut gw_id = self.gateway_id.lock().await;
+        *gw_id = hex::encode(gateway_id);
+    }
+
+    async fn get_gateway_id(&self) -> String {
+        self.gateway_id.lock().await.clone()
+    }
+
+    async fn set_downlink_cache(&self, token: u16, dc: DownlinkCache) {
+        let mut cache = self.downlink_cache.write().await;
+        cache.insert(token, dc);
+    }
+
+    async fn get_downlink_cache(&self, token: u16) -> Option<DownlinkCache> {
+        self.clean_downlink_cache().await;
+        let cache = self.downlink_cache.read().await;
+        cache.get(&token).cloned()
+    }
+
+    async fn clean_downlink_cache(&self) {
+        let mut cache = self.downlink_cache.write().await;
+        cache.retain(|k, v| {
+            if v.expire < SystemTime::now() {
+                trace!("Removing key from cache, key: {}", k);
+                false
+            } else {
+                true
+            }
+        });
+    }
+
+    async fn set_pull_addr(&self, addr: &SocketAddr) {
+        let mut pull_addr = self.pull_addr.write().await;
+        *pull_addr = Some(*addr);
+    }
+
+    async fn get_pull_addr(&self) -> Result<SocketAddr> {
+        self.pull_addr.read().await.ok_or(anyhow!("No pull_addr"))
+    }
+
+    async fn export_stats(&self) -> Result<gw::GatewayStats> {
+        let mut stats = self.stats.lock().await;
+        stats.export_stats()
+    }
+
+    async fn count_uplink(&self, pl: &gw::UplinkFrame) -> Result<()> {
+        let mut stats = self.stats.lock().await;
+        stats.count_uplink(pl)
+    }
+
+    async fn count_downlink(&self, pl: &gw::DownlinkFrame, ack: &gw::DownlinkTxAck) -> Result<()> {
+        let mut stats = self.stats.lock().await;
+        stats.count_downlink(pl, ack)
+    }
+}
+
+pub struct Backend {
+    state: Arc<State>,
+}
+
+impl Backend {
+    pub async fn setup(conf: &Configuration) -> Result<Self> {
+        info!("Setting up Semtech UDP packet-forwarder backend");
+
+        info!(
+            "Binding UDP socket, bind: {}",
+            conf.backend.semtech_udp.bind
+        );
+        let socket = UdpSocket::bind(&conf.backend.semtech_udp.bind).await?;
+
+        // setup state
+        let state = State {
+            socket,
+            gateway_id: Mutex::new(conf.backend.gateway_id.clone()),
+            downlink_cache: RwLock::new(HashMap::new()),
+            pull_addr: RwLock::new(None),
+            stats: Mutex::new(Stats::default()),
+            time_fallback_enabled: conf.backend.semtech_udp.time_fallback_enabled,
+            forward_crc_invalid: conf.backend.filters.forward_crc_invalid,
+            forward_crc_missing: conf.backend.filters.forward_crc_missing,
+            forward_crc_ok: conf.backend.filters.forward_crc_ok,
+            filters: lrwn_filters::Filters {
+                dev_addr_prefixes: conf.backend.filters.dev_addr_prefixes.clone(),
+                join_eui_prefixes: conf.backend.filters.join_eui_prefixes.clone(),
+            },
+        };
+        let state = Arc::new(state);
+
+        tokio::spawn({
+            let state = state.clone();
+            async move {
+                udp_receive_loop(state).await;
+            }
+        });
+
+        Ok(Backend { state })
+    }
+}
+
+#[async_trait]
+impl BackendTrait for Backend {
+    async fn get_gateway_id(&self) -> Result<String> {
+        let gw_id = self.state.get_gateway_id().await;
+        if gw_id.is_empty() {
+            return Err(anyhow!("Gateway ID not yet set"));
+        }
+        Ok(gw_id)
+    }
+
+    async fn send_downlink_frame(&self, pl: &gw::DownlinkFrame) -> Result<()> {
+        let mut acks: Vec<gw::DownlinkTxAckItem> = Vec::with_capacity(pl.items.len());
+        for _ in &pl.items {
+            acks.push(gw::DownlinkTxAckItem {
+                status: gw::TxAckStatus::Ignored.into(),
+            });
+        }
+        send_downlink_frame(&self.state, pl, acks, 0).await
+    }
+
+    async fn send_configuration_command(&self, _pl: &gw::GatewayConfiguration) -> Result<()> {
+        Ok(())
+    }
+}
+
+async fn udp_receive_loop(state: Arc<State>) {
+    let mut buffer: [u8; 65535] = [0; 65535];
+
+    loop {
+        let (size, remote) = match state.socket.recv_from(&mut buffer).await {
+            Ok((size, remote)) => (size, remote),
+            Err(e) => {
+                warn!("UDP socket receive error: {}", e);
+                continue;
+            }
+        };
+
+        if size < 4 {
+            warn!(
+                "At least 4 bytes are expected, received: {}, remote: {}",
+                size, remote
+            );
+            continue;
+        }
+
+        match buffer[3] {
+            0x00 => {
+                // PUSH_DATA
+                if let Err(e) = handle_push_data(&state, &buffer[..size], &remote).await {
+                    warn!("Handle PUSH_DATA error: {}, remote: {}", e, remote);
+                }
+            }
+            0x02 => {
+                // PULL_DATA
+                if let Err(e) = handle_pull_data(&state, &buffer[..size], &remote).await {
+                    warn!("Handle PULL_DATA error: {}, remote: {}", e, remote);
+                }
+            }
+            0x05 => {
+                // TX_ACK
+                if let Err(e) = handle_tx_ack(&state, &buffer[..size], &remote).await {
+                    warn!("Handle TX_ACK error: {}, remote: {}", e, remote);
+                }
+            }
+            _ => {
+                warn!(
+                    "Unexepcted command received, cid: {}, remote: {}",
+                    buffer[3], remote
+                );
+                continue;
+            }
+        }
+    }
+}
+
+async fn handle_push_data(state: &Arc<State>, data: &[u8], remote: &SocketAddr) -> Result<()> {
+    let pl = structs::PushData::from_slice(data)?;
+
+    info!(
+        "PUSH_DATA received, random_token: {}, remote: {}",
+        pl.random_token, remote
+    );
+
+    info!(
+        "Sending PUSH_ACK, random_token: {} remote: {}",
+        pl.random_token, remote
+    );
+    let ack = structs::PushAck {
+        random_token: pl.random_token,
+    };
+    state.socket.send_to(&ack.to_vec(), remote).await?;
+
+    let uplink_frames = pl.to_proto_uplink_frames(state.time_fallback_enabled)?;
+    let gateway_stats = pl.to_proto_gateway_stats()?;
+
+    for uf in &uplink_frames {
+        if let Some(rx_info) = &uf.rx_info {
+            if !((rx_info.crc_status() == gw::CrcStatus::CrcOk && state.forward_crc_ok)
+                || (rx_info.crc_status() == gw::CrcStatus::BadCrc && state.forward_crc_invalid)
+                || (rx_info.crc_status() == gw::CrcStatus::NoCrc && state.forward_crc_missing))
+            {
+                debug!(
+                    "Ignoring uplink frame because of forward_crc_ flags, uplink_id: {}",
+                    uf.rx_info.as_ref().map(|v| v.uplink_id).unwrap_or_default(),
+                );
+
+                continue;
+            }
+        }
+
+        if lrwn_filters::matches(&uf.phy_payload, &state.filters) {
+            state.count_uplink(uf).await?;
+            send_uplink_frame(uf).await?;
+        } else {
+            debug!(
+                "Ignoring uplink frame because of dev_addr and join_eui filters, uplink_id: {}",
+                uf.rx_info.as_ref().map(|v| v.uplink_id).unwrap_or_default()
+            );
+        }
+    }
+
+    if let Some(mut stats) = gateway_stats {
+        let s = state.export_stats().await?;
+        stats.rx_packets_received_ok = s.rx_packets_received_ok;
+        stats.tx_packets_emitted = s.tx_packets_emitted;
+        stats.rx_packets_per_frequency = s.rx_packets_per_frequency.clone();
+        stats.tx_packets_per_frequency = s.tx_packets_per_frequency.clone();
+        stats.rx_packets_per_modulation = s.rx_packets_per_modulation.clone();
+        stats.tx_packets_per_modulation = s.tx_packets_per_modulation.clone();
+        stats.tx_packets_per_status = s.tx_packets_per_status.clone();
+        stats.metadata.extend(metadata::get().await?);
+
+        send_gateway_stats(&stats).await?;
+    }
+
+    Ok(())
+}
+
+async fn handle_pull_data(state: &Arc<State>, data: &[u8], remote: &SocketAddr) -> Result<()> {
+    let pl = structs::PullData::from_slice(data)?;
+
+    info!(
+        "PULL_DATA received, random_token: {}, remote: {}",
+        pl.random_token, remote
+    );
+
+    info!(
+        "Sending PULL_ACK, random_token: {}, remote: {}",
+        pl.random_token, remote
+    );
+    let ack = structs::PullAck {
+        random_token: pl.random_token,
+    };
+    state.socket.send_to(&ack.to_vec(), remote).await?;
+
+    // Set the Gateway ID.
+    state.set_gateway_id(&pl.gateway_id).await;
+    // Store the address from which the PULL_DATA is coming, and to which we need to respond with
+    // PULL_RESP in case we have any data to send.
+    state.set_pull_addr(remote).await;
+
+    Ok(())
+}
+
+async fn handle_tx_ack(state: &Arc<State>, data: &[u8], remote: &SocketAddr) -> Result<()> {
+    let pl = structs::TxAck::from_slice(data)?;
+
+    info!(
+        "TX_ACK received, random_token: {}, remote: {}, error: {}",
+        pl.random_token,
+        remote,
+        pl.payload
+            .as_ref()
+            .cloned()
+            .unwrap_or_default()
+            .txpk_ack
+            .error
+    );
+
+    let downlink_cache = state
+        .get_downlink_cache(pl.random_token)
+        .await
+        .ok_or_else(|| anyhow!("No cache item for token, random_token: {}", pl.random_token))?;
+
+    let ack_status = pl.to_proto_tx_ack_status();
+
+    let mut ack_items = downlink_cache.ack_items.clone();
+    ack_items[downlink_cache.index].status = ack_status.into();
+
+    if ack_status == gw::TxAckStatus::Ok || downlink_cache.index >= ack_items.len() - 1 {
+        let pl = gw::DownlinkTxAck {
+            gateway_id: hex::encode(pl.gateway_id),
+            downlink_id: downlink_cache.frame.downlink_id,
+            items: ack_items,
+            ..Default::default()
+        };
+        state.count_downlink(&downlink_cache.frame, &pl).await?;
+        send_tx_ack(&pl).await
+    } else {
+        send_downlink_frame(
+            state,
+            &downlink_cache.frame,
+            ack_items,
+            downlink_cache.index + 1,
+        )
+        .await
+    }
+}
+
+async fn send_downlink_frame(
+    state: &Arc<State>,
+    pl: &gw::DownlinkFrame,
+    acks: Vec<gw::DownlinkTxAckItem>,
+    i: usize,
+) -> Result<()> {
+    let token = pl.downlink_id as u16;
+    state
+        .set_downlink_cache(
+            token,
+            DownlinkCache {
+                expire: SystemTime::now() + Duration::from_secs(60),
+                frame: pl.clone(),
+                ack_items: acks,
+                index: i,
+            },
+        )
+        .await;
+
+    let pull_resp = structs::PullResp::from_proto(pl, i, token)?;
+    let pull_addr = state.get_pull_addr().await?;
+
+    info!(
+        "Sending PULL_RESP, random_token: {}, remote: {}",
+        token, pull_addr
+    );
+
+    state
+        .socket
+        .send_to(&pull_resp.to_vec()?, pull_addr)
+        .await?;
+
+    Ok(())
+}
