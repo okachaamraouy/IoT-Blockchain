6ceaf9086746deea379567e7f47417a2b019bcfe
Okacha Amraouy <okacha.amraouy@usmba.ac.ma>
2024-10-24 15:53:06 +0100

    Update 2IaaS-NFT-Token.sol

--- SC2/2IaaS-NFT-Token.sol
+++ SC2/2IaaS-NFT-Token.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// SmartIrrigationToken as an ERC-20 Token
+contract SmartIrrigationToken is ERC20, Ownable {
+
+    // Constructor to set the token name and symbol, and set the initial owner
+    constructor() ERC20("Smart Irrigation Token", "2IaaS") Ownable(msg.sender) {
+        // Mint initial supply to the contract owner (e.g., government authority)
+        _mint(msg.sender, 10e18 * 10 ** decimals()); // Mint 100 trillion tokens initially
+    }
+
+    // Function to mint new tokens (e.g., reward for water-efficient irrigation)
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Function to burn tokens (reduce total supply)
+    function burnTokens(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
