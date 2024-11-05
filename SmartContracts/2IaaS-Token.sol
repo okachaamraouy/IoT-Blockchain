3a1e2b04edb35a7f5547e4e67be74b5a4f492f72
Okacha Amraouy <okacha.amraouy@usmba.ac.ma>
2024-11-05 16:05:34 +0100

    Update 2IaaS-Token.sol

--- SmartContracts/2IaaS-Token.sol
+++ SmartContracts/2IaaS-Token.sol
@@ -7,10 +7,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // SmartIrrigationToken as an ERC-20 Token
 contract SmartIrrigationToken is ERC20, Ownable {
 
-    // Constructor to set the token name and symbol, and set the initial owner
+    // Constructor to set the token name and symbol, and mint the initial supply to the owner
     constructor() ERC20("Smart Irrigation Token", "2IaaS") Ownable(msg.sender) {
         // Mint initial supply to the contract owner (e.g., government authority)
-        _mint(msg.sender, 10e18 * 10 ** decimals()); // Mint 100 trillion tokens initially
+        _mint(msg.sender, 100e18 * 10 ** decimals()); // Mint trillion tokens initially
     }
 
     // Function to mint new tokens (e.g., reward for water-efficient irrigation)
@@ -18,7 +18,7 @@ contract SmartIrrigationToken is ERC20, Ownable {
         _mint(to, amount);
     }
 
-    // Function to burn tokens (reduce total supply)
+    // Function to burn tokens from a specific address (only owner can execute this)
     function burnTokens(address from, uint256 amount) public onlyOwner {
         _burn(from, amount);
     }
